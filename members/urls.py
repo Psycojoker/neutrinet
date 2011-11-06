f3a55d3f977d6d540711bf12562a67148d05a06e
Laurent Peuch <cortex@worlddomination.be>
2011-11-07 00:09:07 +0100

    [enh] list the adhesion demands

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic import ListView
 
+from models import Member
 from views import AdhesionFormView
 
 urlpatterns = patterns('members.views',
     url(r'^adhesion/demande/$', AdhesionFormView.as_view(), name="adherer"),
     url(r'^adhesion/demande/confirm/$', 'adherer', name="adherer_post"),
+    url(r'^adhesion/list/$', ListView.as_view(queryset=Member.objects.filter(demande__isnull=False)), name="adherer_post"),
 )
