615f2e43a7c7409b7d4a51504323efa9f2213642
Laurent Peuch <cortex@worlddomination.be>
2011-11-07 00:47:15 +0100

    [mod] rename the url for listing awaiting adhesion demandes

--- members/urls.py
+++ members/urls.py
@@ -7,5 +7,5 @@ from views import AdhesionFormView
 urlpatterns = patterns('members.views',
     url(r'^adhesion/demande/$', AdhesionFormView.as_view(), name="adherer"),
     url(r'^adhesion/demande/confirm/$', 'adherer', name="adherer_post"),
-    url(r'^adhesion/list/$', ListView.as_view(queryset=Member.objects.filter(demande__isnull=False)), name="adherer_post"),
+    url(r'^adhesion/list/$', ListView.as_view(queryset=Member.objects.filter(demande__isnull=False)), name="adherer_list"),
 )
