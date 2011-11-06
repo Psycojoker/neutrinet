3f1e07faea4f38f8d24106d81a1b9cd87bd18309
Laurent Peuch <cortex@worlddomination.be>
2011-11-07 00:04:02 +0100

    [mod] change urls patterns of members app

--- members/urls.py
+++ members/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 from views import AdhesionFormView
 
 urlpatterns = patterns('members.views',
-    url(r'^adherer/$', AdhesionFormView.as_view(), name="adherer"),
-    url(r'^adherer/post/$', 'adherer', name="adherer_post"),
+    url(r'^adhesion/demande/$', AdhesionFormView.as_view(), name="adherer"),
+    url(r'^adhesion/demande/confirm/$', 'adherer', name="adherer_post"),
 )
