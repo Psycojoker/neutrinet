a8d97ad1c0b2a8c253ddf332205bcc2a56de2563
Laurent Peuch <cortex@worlddomination.be>
2011-11-07 00:01:25 +0100

    [mod] add members in installed apps

--- settings.py
+++ settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.flatpages',
+    'members',
 )
 
 # A sample logging configuration. The only tangible logging
