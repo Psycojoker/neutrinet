235eafa4fd0c28772d82f12d78a5a26c7524f22b
Laurent Peuch <cortex@worlddomination.be>
2011-11-13 20:24:12 +0100

    [enh] protect listing page so only admin can see it

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import ListView
+from django.contrib.auth.decorators import user_passes_test
 
 from models import Member
 from views import AdhesionFormView
@@ -7,5 +8,5 @@ from views import AdhesionFormView
 urlpatterns = patterns('members.views',
     url(r'^adhesion/demande/$', AdhesionFormView.as_view(), name="adherer"),
     url(r'^adhesion/demande/confirm/$', 'adherer', name="adherer_post"),
-    url(r'^adhesion/list/$', ListView.as_view(queryset=Member.objects.filter(demande__isnull=False)), name="adherer_list"),
+    url(r'^adhesion/list/$', user_passes_test(lambda u: u.is_superuser, "/admin/")(ListView.as_view(queryset=Member.objects.filter(demande__isnull=False))), name="adherer_list"),
 )
