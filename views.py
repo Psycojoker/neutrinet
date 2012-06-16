7cc0a5f58857330dd2ce7768f7f52df05e2329f1
Laurent Peuch <cortex@worlddomination.be>
2012-06-16 19:30:41 +0200

    [mod] make sure that request on tweets is ajax

--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 from feedparser import parse
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
 
 def tweets(request):
+    if not request.is_ajax():
+        return HttpResponseBadRequest()
     rss = parse('https://identi.ca/api/statuses/user_timeline/292360.rss')
     return render(request, 'tweets.html', {"tweets": rss.entries[:7]})
